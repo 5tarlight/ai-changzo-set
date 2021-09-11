a78d4eb63d692975113e34d7f63ec231541b53d1
5tarlight <developerstarlight@gmail.com>
2021-09-12 01:07:38 +0900

    use onChnage

--- src/components/Header/ClassInput.tsx
+++ src/components/Header/ClassInput.tsx
@@ -11,36 +11,34 @@ const Input = styled.input`
 `
 
 const ClassInput: FC<Props> = ({ value, setValue }) => {
-  // const updateValue = (v: string) => {
-  //   const e = v.replace(/[^0-9]/g, '')
-  //   setValue(e.slice(0, Math.min(5, e.length)))
-
-  //   console.log(value, value.length)
-  //   if (value.length === 5) setClassNo(value)
-  // }
-
-  const append = (key: string) => {
-    if (value.length === 5) return
-    if (value.length === 0 && !(key === '1' || key === '2' || key === '3'))
-      return
-    setValue(value + key)
+  const updateValue = (v: string) => {
+    if (value.length === 0 && !(v === '1' || v === '2' || v === '3')) return
+    const e = v.replace(/[^0-9]/g, '')
+    setValue(e.slice(0, Math.min(5, e.length)))
   }
-  const remove = () => setValue(value.slice(0, value.length - 1))
+
+  // const append = (key: string) => {
+  //   if (value.length === 5) return
+  //   if (value.length === 0 && !(key === '1' || key === '2' || key === '3'))
+  //     return
+  //   setValue(value + key)
+  // }
+  // const remove = () => setValue(value.slice(0, value.length - 1))
 
   return (
     <Input
       placeholder="학번"
       value={value}
-      // onChange={({ target: { value } }) => {
-      //   updateValue(value)
-      // }}
-      onChange={() => {}} // to disable devtool warning
-      onKeyDown={e => {
-        const { key } = e
-        const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-        if (nums.includes(key)) append(key)
-        else if (key == 'Backspace') remove()
+      onChange={({ target: { value } }) => {
+        updateValue(value)
       }}
+      // onChange={() => {}} // to disable devtool warning
+      // onKeyDown={e => {
+      //   const { key } = e
+      //   const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+      //   if (nums.includes(key)) append(key)
+      //   else if (key == 'Backspace') remove()
+      // }}
     />
   )
 }
