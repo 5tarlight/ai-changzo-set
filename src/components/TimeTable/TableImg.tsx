d37eca896d92d6693ee8ff52daa43f3c9bb21705
5tarlight <developerstarlight@gmail.com>
2021-10-14 12:34:08 +0900

    timetable image

--- src/components/TimeTable/TableImg.tsx
+++ src/components/TimeTable/TableImg.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import styled from 'styled-components'
+import { isValidId } from '../../util'
 import g1c3 from './img/1-3.jpg'
 
 interface Props {
@@ -12,16 +13,24 @@ const imgs = [['', '', g1c3, '', '', '', '', '', '', ''], [], []]
 const Image = styled.div`
   width: 500px;
   height: 500px;
+  float: right;
+  margin-right: 50px;
+  margin-top: -130px;
 `
 
 const TableImg: FC<Props> = ({ grade, clazz }) => {
-  const imgBackStyle = {
-    background: `rgba(0, 0, 0, .65) url(${imgs[grade - 1][clazz - 1]})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
-    backgroundBlendMode: 'darken',
-  }
+  let imgBackStyle = {}
+  if (isValidId({ grade, clazz, no: 1 }))
+    imgBackStyle = {
+      background: `rgba(0, 0, 0, .65) url(${imgs[grade - 1][clazz - 1]})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+    }
+  else
+    imgBackStyle = {
+      display: 'none',
+    }
 
   return <Image style={imgBackStyle} />
 }
